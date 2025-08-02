3db7af40595207af26e32c599d3b909bdf7ebd7e
ghasem <ghasem.fattahpour@gmail.com>
2025-08-02 16:34:36 -0400

    Release 1.0.0

--- src/main/java/com/fattahpour/ksra/KsraApplication.java
+++ src/main/java/com/fattahpour/ksra/KsraApplication.java
@@ -3,13 +3,11 @@ package com.fattahpour.ksra;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/**
- * Entry point for the Kafka Schema Registry Analytics application.
- */
 @SpringBootApplication
 public class KsraApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(KsraApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(KsraApplication.class, args);
+	}
+
 }

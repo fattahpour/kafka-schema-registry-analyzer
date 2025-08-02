95bdcf0067a9ff686a1439ce4f90ee0344482929
Ghasem Fattahpour <ghasem.fattahpour@gmail.com>
2025-08-02 14:22:43 -0400

    docs: add swagger configuration for controllers

---
+++ src/main/java/com/fattahpour/ksra/controller/SchemaRegistryController.java
@@ -0,0 +1,69 @@
+package com.fattahpour.ksra.controller;
+
+import com.fattahpour.ksra.model.RegisterSchemaRequest;
+import com.fattahpour.ksra.service.SchemaRegistryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+/**
+ * WebFlux controller exposing basic Schema Registry operations.
+ */
+@RestController
+@Tag(name = "Schema Registry")
+public class SchemaRegistryController {
+
+    private final SchemaRegistryService service;
+
+    public SchemaRegistryController(SchemaRegistryService service) {
+        this.service = service;
+    }
+
+    @Operation(summary = "List all schema subjects")
+    @ApiResponse(responseCode = "200", description = "Array of subject names")
+    @GetMapping("/subjects")
+    public Flux<String> listSubjects() {
+        return service.getSubjects();
+    }
+
+    @Operation(summary = "List versions of a subject")
+    @ApiResponse(responseCode = "200", description = "Array of version numbers")
+    @GetMapping("/subjects/{subject}/versions")
+    public Flux<Integer> listVersions(
+            @Parameter(description = "Subject name")
+            @PathVariable String subject) {
+        return service.getVersions(subject);
+    }
+
+    @Operation(summary = "Fetch schema by global ID")
+    @ApiResponse(responseCode = "200", description = "Schema string")
+    @GetMapping("/schemas/ids/{id}")
+    public Mono<String> getSchemaById(
+            @Parameter(description = "Global schema id")
+            @PathVariable int id) {
+        return service.getSchemaById(id);
+    }
+
+    @Operation(summary = "Register a new Avro schema under a subject")
+    @ApiResponse(responseCode = "200", description = "Assigned schema id",
+            content = @Content(schema = @Schema(implementation = Integer.class)))
+    @PostMapping(value = "/subjects/{subject}/versions", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<Integer> registerSchema(
+            @Parameter(description = "Subject name")
+            @PathVariable String subject,
+            @RequestBody RegisterSchemaRequest request) {
+        return service.registerSchema(subject, request.schema());
+    }
+}
+

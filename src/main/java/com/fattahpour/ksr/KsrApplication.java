f5020b10d502e636b4b11fca4735d94dcda36cdf
ghasem <ghasem.fattahpour@gmail.com>
2025-08-02 13:07:22 -0400

    Init commit

---
+++ src/main/java/com/fattahpour/ksr/KsrApplication.java
@@ -0,0 +1,13 @@
+package com.fattahpour.ksr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KsrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KsrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fattahpour/ksr/KsrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fattahpour.ksr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KsrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
